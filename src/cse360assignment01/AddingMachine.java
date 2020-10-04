89c8bccec31ee5d1b0ce07c5087cbb74d9a69e8c
sohan <sbyrapun@asu.edu>
2020-10-03 17:26:30 -0700

    First turn in

---
+++ src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment01;
+
+public class AddingMachine {
+		private int total;
+		 
+		  public AddingMachine () {
+		    total = 0;  // not needed - included for clarity
+		  }
+		  //Returns total value
+		  public int getTotal () {
+		    return 0;
+		  }
+		  //Changes the value of total by adding value
+		  public void add (int value) {
+		  }
+		  //Changes the value of total by subtracting value
+		  public void subtract (int value) {
+		  }
+		  //Return a value that displays all the operations
+		  public String toString () {
+		    return "";
+		  }
+		  //Clears all the operations displayed
+		  public void clear() {
+		  }
+		}
